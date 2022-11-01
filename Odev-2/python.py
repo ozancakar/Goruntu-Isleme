d6095b8c098b5fc14d9063425247ee835b2b03e6
Ozan <ozan.1071@hotmail.com>
2022-11-02 01:17:09 +0300

    Goruntunun Negatifinin Alınması
    PIL kütüphanesini kullanarak görüntünün negatifini alma

---
+++ Odev-2/python.py
@@ -0,0 +1,21 @@
+from PIL import Image
+
+
+img = Image.open("./tavus.jpg");
+
+img.show()
+
+for i in range(0, img.size[0] - 1):
+
+    for j in range(0, img.size[1] - 1):
+        pixelColorVals = img.getpixel((i, j));
+
+        redPixel = 255 - pixelColorVals[0];  # Negate red pixel
+
+        greenPixel = 255 - pixelColorVals[1];  # Negate green pixel
+
+        bluePixel = 255 - pixelColorVals[2];  # Negate blue pixel
+
+        img.putpixel((i, j), (redPixel, greenPixel, bluePixel));
+
+img.show();
